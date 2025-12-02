de4401fb5c4c686398f51df7eb700e3793cb0495
PedroLuiz02 <pedroluiz0034@gmail.com>
2025-12-02 03:48:39 -0300

    Update README

--- main.py
+++ main.py
@@ -5,11 +5,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-# Rodar com ( python main.py ) dar pip install em Flask, Flask-login, e Flask-SqlAlchemy
-
-# Email Teste: silvanno198@gmail.com
-# Senha Teste: Silvanno198.
-
-# Email Teste: roberto1@gmail.com
-# Senha Teste: Rroberto1.
